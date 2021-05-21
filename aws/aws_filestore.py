30f0bd3a1cb973c7457db23091c0377b50511183
Hyeonghak <qorgudgkr@naver.com>
2021-05-21 13:11:23 +0900

    second commit

--- aws/aws_filestore.py
+++
@@ -1,9 +0,0 @@
-import os 
-import glob
-input_path = "/home/pi/Desktop/picture/"
-files        = glob.glob(os.path.join(input_path,'*'))
-stored_names =  list(map(lambda x: x.split("\")[1], files))
-
-for file,name in zip(files,stored_names):
-    s3.upload_file(file,"BUCKET_NAME_YOU_WANT",name)
-
